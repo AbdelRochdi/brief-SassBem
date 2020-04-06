5c56d16b1f052b5ce4f4001bd424d7e54001841e
AbdelRochdi <abdelghafour.rochdi96@gmail.com>
2020-04-06 22:54:46 +0100

    javascript almost complete

--- app.js
+++ app.js
@@ -12,3 +12,43 @@ const navSlide = () => {
 };
 
 navSlide();
+
+const featuresList = document.querySelector('.features__list');
+const featuresItems = document.querySelectorAll('.features__list li');
+const featuresArticles = document.querySelectorAll('.features__articles');
+
+featuresList.addEventListener('click', changeFeature);
+
+function changeFeature(e) {
+	console.log(featuresArticles);
+	if (e.target.classList.contains('bar')) {
+		return;
+	}
+
+	featuresItems.forEach((item, index) => {
+		if (item === e.target) {
+			item.lastChild.style.display = 'block';
+			featuresArticles[index].style.display = 'block';
+		} else {
+			item.lastChild.style.display = 'none';
+			featuresArticles[index].style.display = 'none';
+		}
+	});
+}
+
+const questions = document.querySelector('.questions__menu');
+
+questions.addEventListener('click', slideText);
+
+function slideText(e) {
+	console.log(e.target);
+	console.log(e.target.parentElement);
+	if (e.target.classList.contains('accordion')) {
+		if (e.target.parentElement.lastElementChild.style.display === 'block') {
+			e.target.parentElement.lastElementChild.style.display = 'none';
+		} else {
+			e.target.parentElement.lastElementChild.style.display = 'block';
+		}
+	}
+	e.preventDefault();
+}
